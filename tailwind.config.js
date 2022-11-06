346a865ba3fc965916a62eda9091b46cbcfe32df
Saleem <saleemismail14@yahoo.com>
2022-11-06 16:15:54 +0100

    Add self-affirmation page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./views/**/*.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow:{
+        'custom': `0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1), 
+        0 -5px 15px -3px rgb(0 0 0 / 0.1), 0 -4px 6px -4px rgb(0 0 0 / 0.1)`
+      }
+    },
     colors: {
       "primary-90":"#163725",
       "primary-70":"#255C3D",
@@ -18,7 +23,11 @@ module.exports = {
       "icon":"#6C6F6D",
       "gray-50": "#464646",
       "gray-40": "#585858",
-      "error": "#BF5E5E"
+      "error": "#BF5E5E",
+      "card-border": "#E6F6ED",
+      "semantics": "#840B0B",
+      "modal": "#667085",
+      "black": "#000000"
 
     },
     fontFamily: {      
