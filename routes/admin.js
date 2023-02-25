1360483b3d3a6886e5d9ba2a2a6ff36da1452406
qoudri4re <qoudri4re@gmail.com>
2023-02-25 16:40:18 +0100

    routes for admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,2 +1,29 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+//home, self-affirmation, mental-health-tips, forum, blog, feedback
+
+router.get("/home", (req, res) => {
+  res.render("pages/admin/home");
+});
+
+router.get("/self-affirmation", (req, res) => {
+  res.render("pages/admin/self-affirmation");
+});
+
+router.get("/mental-health-tips", (req, res) => {
+  res.render("pages/admin/mental-health-tips");
+});
+
+router.get("/forum", (req, res) => {
+  res.render("pages/admin/forum");
+});
+
+router.get("/blog", (req, res) => {
+  res.render("pages/admin/blog");
+});
+
+router.get("/feedback", (req, res) => {
+  res.render("pages/admin/feedback");
+});
+
+module.exports = router;
